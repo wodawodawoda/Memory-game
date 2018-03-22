f6113f45c330500732061c57c1d719bfaa53f1c4
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-22 23:16:52 +0100

    Initial commit

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
