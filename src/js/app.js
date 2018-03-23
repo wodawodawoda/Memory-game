ade492ea467834053de75b0033d9958cbf02b0d6
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-23 17:35:22 +0100

    Game is working propely TOFIX: setTimeout can't hold last from getting new assign

--- src/js/app.js
+++ src/js/app.js
@@ -36,6 +36,7 @@ let icons = [
 let iconsDouble = [...icons, ...icons];
 
 const div = document.createElement('div');
+const figure = document.createElement('figure');
 
 function createGame(...icons) {
     shuffle(icons);
@@ -44,7 +45,13 @@ function createGame(...icons) {
     gameField.id = 'game-field';
     for (let i = 0; i < icons.length; i++) {
         const card = div.cloneNode();
-        card.classList = `game-field__card fa fa-${icons[i]}`;
+        const front = figure.cloneNode();
+        const back = figure.cloneNode();
+        card.appendChild(front);
+        card.appendChild(back);
+        card.classList = `game-field__card`;
+        front.classList = `game-field__card--front`;
+        back.classList = `game-field__card--back fa fa-${icons[i]}`;
         card.setAttribute('name', icons[i])
         gameField.appendChild(card);
     }
@@ -54,26 +61,65 @@ createGame(...iconsDouble);
 
 const $gameField = document.getElementById('game-field');
 let counter = 0;
-let divStorage = {};
+let last;
 const open = 'game-field__card--open';
 const correct = 'game-field__card--correct';
 const incorrect = 'game-field__card--incorrect';
+const reset = 'game-field__card--reset'
 
+$gameField.addEventListener('click', getValues);
 
-$gameField.addEventListener('click', (e) => {
+function getValues(e) {
+    // if(last === e.target.parentNode) {return}
     counter++;
-    // if(counter % 2 !== 0 && counter > 2) {divStorage = {}} else {divStorage = e.target};
-    if(counter % 2 !== 0) {divStorage = e.target}
-    else {
-        if(e.target.attributes.name.value === divStorage.attributes.name.value) {
-            e.target.classList.add(correct);
-            divStorage.classList.add(correct);
-        }
-        divStorage = {}
-    }
-    console.log(divStorage)
+    // console.log(e)
+    e.target.parentNode.classList.add('flip');
+    if(last && counter % 2 === 0) {
+        console.log('notempty')
+        if(last.attributes.name.value === e.target.parentNode.attributes.name.value){
+            console.log('match')
+            e.target.nextElementSibling.classList.remove('game-field__card--back');
+            e.target.nextElementSibling.classList.remove('game-field__card--back');
+            e.target.nextElementSibling.classList.add('correct');
+        } else {
+            console.log('not match');
+            console.log(last)
+            setTimeout(() => {
+                e.target.parentNode.classList.remove('flip');
+                console.log(last)
+                last.classList.remove('flip');
+            }, 800)
 
-});
+        }
+    } else {
+        console.log('empty')
+    }
+    setTimeout(() => {last = e.target.parentNode}, 800 );
+
+}
+
+// $gameField.addEventListener('click', (e) => {
+//     if(divStorage === e.target) {return}
+//     counter++;
+//         // divStorage.classList.remove(reset);
+//         e.target.classList.remove(reset);
+//     console.log(divStorage);
+//     if(counter % 2 !== 0) {
+//         e.target.classList.add(open);
+//         divStorage = e.target
+//     } else {
+//         if(e.target.attributes.name.value === divStorage.attributes.name.value) {
+//             e.target.classList.add(correct);
+//             divStorage.classList.add(correct);
+//         } else {
+//             divStorage.classList.add(reset);
+//             e.target.classList.add(reset);
+//         }
+//         e.target.classList.remove(open);
+//         divStorage = undefined
+//     }
+// });
+//SEPARATE FUNCTION TO COMPARE TWO COMPARE TWO VALUES
 
 // $gameField.addEventListener('click', (e) => {
 //     console.log(divStorage)
